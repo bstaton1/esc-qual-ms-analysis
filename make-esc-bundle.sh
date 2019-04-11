ad9cb5813851e14d836331b0c05cb80013345b14
Ben Staton <statonbe@gmail.com>
2019-04-11 08:33:12 -0500

    create a make bundle shell script
    Accepts specific model numbers as input and bundles the saved output into a tar.gz. MUST be executed from the directory that contains model-files (i.e., currently one directory up from this repository)

---
+++ make-esc-bundle.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir esc-qual-bundle
+cd model-output
+
+echo "Models with completed output:"
+ls post* | grep -o [0-9]*
+
+echo "---------------------------------------"
+echo "Which models do you want to bundle?"
+echo "  (separated by spaces):"
+read models
+echo "---------------------------------------"
+
+echo "Making Copies"
+for model in ${models[@]}
+do
+  echo "  Model $model Output"
+  cp *-$model.rds ../esc-qual-bundle
+done
+
+cd ../
+
+echo "Creating Tarball:"
+tar -cvzf esc-qual-bundle.tar.gz esc-qual-bundle
+
+rm -r esc-qual-bundle
