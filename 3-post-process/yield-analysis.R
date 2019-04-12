bb0a4d557f5e48f06945f977a81d80bef74368ee
Ben Staton <statonbe@gmail.com>
2019-04-11 19:16:01 -0500

    update Smsc comparison plots

--- 3-post-process/yield-analysis.R
+++ 3-post-process/yield-analysis.R
@@ -10,48 +10,65 @@ source("../new-func-source.R")
 out_dir = "../../model-output"
 out_files = dir(out_dir)
 
-msy_files = file.path(out_dir, out_files[stringr::str_detect(out_files, "msy")])
+msyfiles = out_files[stringr::str_detect(out_files, "msy")]
+goalsfiles = out_files[stringr::str_detect(out_files, "goals")]
+mods = stringr::str_extract(msyfiles, "[0-9]+")
 
-i = 1
+msy = readRDS(file.path(out_dir, msyfiles[1]))
+goals = readRDS(file.path(out_dir, goalsfiles[1]))$EG
 
-msys = readRDS(msy_files[1])
-for (i in 2:length(msy_files)) {msys = abind(msys, readRDS(msy_files[i]), along = 5)}
+for (i in 2:length(msyfiles)) msy = abind::abind(msy, readRDS(file.path(out_dir, msyfiles[i])), along = 5) 
+for (i in 2:length(goalsfiles)) goals = abind::abind(goals, readRDS(file.path(out_dir, goalsfiles[i]))$EG, along = 5) 
+
+dimnames(msy)[[5]] = mods
+dimnames(goals)[[5]] = mods
+
+keep_mods = c("2", "7", "10", "12", "13", "15", "16", "17", "18")
+
+
+msy = msy[,,,,keep_mods]
 
 value = "esc"
 vuln = "res"
 q = c("2.5%", "25%", "50%", "75%", "97.5%")
 
-early = msys[,value,vuln,"early",]
-all = msys[,value,vuln,"all",]
-late = msys[,value,vuln,"late",]
+early = msy[,value,vuln,"early",]
+all = msy[,value,vuln,"all",]
+late = msy[,value,vuln,"late",]
 
 # windows()
 
 q_lwr = "10%"; q_upr = "90%"
-xall = 1:6
-xearly = xall - 0.15
-xlate = xall + 0.15
+xall = 1:length(keep_mods)
+xearly = xall - 0.2
+xlate = xall + 0.2
 ppi = 600
 png(paste(vuln, ".png", sep = ""), h = 5 * ppi, w = 5 * ppi, res = ppi)
-par(mar = c(6,3.5,2,2), tcl = -0.25, mgp = c(2,0.5,0))
-plot(all["50%",] ~ xall, yaxt = "n", xlim = range(xall) + c(-0.15,0.15), xaxt = "n", ylim = range(msys[c(q_lwr, q_upr),value,,,]), pch = 0,
+par(mar = c(8,3.5,2,2), tcl = -0.25, mgp = c(2,0.5,0))
+plot(all["50%",] ~ xall, yaxt = "n", xlim = range(xall) + c(-0.15,0.15), xaxt = "n", ylim = range(msy[c(q_lwr, q_upr),value,,,]), pch = 0,
      xlab = "", ylab = "", main = ifelse(vuln == "unr", "Unrestricted", "Restricted"), cex = 1.2)
 points(early["50%",] ~ xearly, pch = 0, col = "grey", cex = 1.2)
 points(late["50%",] ~ xlate, pch = 15, cex = 1.2)
 segments(xall, all[q_lwr,], xall, all[q_upr,], lty = 2)
 segments(xearly, early[q_lwr,], xearly, early[q_upr,], col = "grey")
 segments(xlate, late[q_lwr,], xlate, late[q_upr,])
-mtext(side = 1, "Model (Spawning Units/Trends)", line = 4.5, font = 2)
+mtext(side = 1, "Model (Spawning Units/Trends)", line = 6.5, font = 2)
 mtext(side = 2, "Smsc", line = 2.5, font = 2)
 axis(side = 1, at = xall, las = 2,
-     labels = c("Body/All", "Eggs/Egg",
-                "Eggs/Sex", "Eggs/Mat", 
-                "Eggs/Vuln", "Eggs/All") 
+     labels = c("Fish/All",
+                "Eggs/None",
+                "Eggs/Eggs",
+                "Eggs/Sex", 
+                "Eggs/Mat",
+                "Eggs/Eggs-Mat",
+                "Eggs/Eggs-Sex",
+                "Eggs/Sex-Mat",
+                "Eggs/All") 
 )
 
 axis(side = 2, at = seq(0, 200000, ifelse(vuln == "unr", 20000, 10000)), labels = seq(0, 200, ifelse(vuln == "unr", 20, 10)), las = 2)
 
-legend("top", horiz = T,
+legend(ifelse(vuln == "unr", "bottom", "top"), horiz = T,
        legend = c("Early 10Y", "All", "Late 10Y"),
        pch = c(0, 0, 15), lty = c(1,2,1), col = c("grey", "black", "black"),
        pt.cex = 1.2, cex = 0.8, bg = "grey95"
