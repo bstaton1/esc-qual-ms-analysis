5c1c16acc27b08d97cc2383398e69e6695b75725
Ben Staton <statonbe@gmail.com>
2019-04-20 19:07:24 -0500

    improvements to get_EG
    NAs were inappropriately used for zeros when calculating probability profiles. This has been fixed, and a curve for the probability that surplus will exceed 100K at each S is returned now as well.

--- 0-functions/get_EG.R
+++ 0-functions/get_EG.R
@@ -3,15 +3,17 @@ get_EG = function(post.samp, spawn_units, vuln, Rmax_p = 0.9, Rmax_prob = 0.9,
                   silent = F) {
   
   # will calculate equilibrium yield, escapement, and recruitment at each F
-  nF = 1000
-  F_range = seq(0.0001, 5, length = nF)
+  nU = 1000
+  U_range = seq(0.0001, 0.9999, length = nU)
+  F_range = log(1/(1 - U_range))
   
   # number of posterior samples
   n_samp = nrow(post.samp)
   
   # containers for output
-  Rout = matrix(NA, n_samp, length(S_breaks) - 1); colnames(Rout) = 1:(length(S_breaks) - 1)
-  Hout = matrix(NA, n_samp, length(S_breaks) - 1); colnames(Hout) = 1:(length(S_breaks) - 1)
+  Rout = matrix(0, n_samp, length(S_breaks) - 1); colnames(Rout) = 1:(length(S_breaks) - 1)
+  Hout = matrix(0, n_samp, length(S_breaks) - 1); colnames(Hout) = 1:(length(S_breaks) - 1)
+  H100out = matrix(0, n_samp, length(S_breaks) - 1); colnames(H100out) = 1:(length(S_breaks) - 1)
   
   for (i in 1:n_samp) {
     if (!silent) cat("\r","      ", round(i/n_samp, 2) * 100, "%", sep = "")
@@ -34,25 +36,29 @@ get_EG = function(post.samp, spawn_units, vuln, Rmax_p = 0.9, Rmax_prob = 0.9,
     tmp = cbind(
       tmp, 
       Rmeet = ifelse(tmp[,"recruits"] > (Rmax_p * max(tmp[,"recruits"])), 1, 0),
-      Hmeet = ifelse(tmp[,"harv"] > (msy_p * max(tmp[,"harv"])), 1, 0)
+      Hmeet = ifelse(tmp[,"harv"] > (msy_p * max(tmp[,"harv"])), 1, 0),
+      H100meet = ifelse(tmp[,"harv"] > 100000, 1, 0)
     )
     
-    Rtmp = tapply(tmp[,"Rmeet"], tmp[,"esc_bin"], function(x) as.numeric(all(x == 1)))
-    Htmp = tapply(tmp[,"Hmeet"], tmp[,"esc_bin"], function(x) as.numeric(all(x == 1)))
+    Rtmp = tapply(tmp[,"Rmeet"], tmp[,"esc_bin"], function(x) as.numeric(any(x == 1)))
+    Htmp = tapply(tmp[,"Hmeet"], tmp[,"esc_bin"], function(x) as.numeric(any(x == 1)))
+    H100tmp = tapply(tmp[,"H100meet"], tmp[,"esc_bin"], function(x) as.numeric(any(x == 1)))
     
     Rout[i,names(Rtmp)] = Rtmp
     Hout[i,names(Htmp)] = Htmp
+    H100out[i,names(Htmp)] = H100tmp
   }
   if (!silent) cat("\n")
   
   pR = colMeans(Rout, na.rm = T)
   pH = colMeans(Hout, na.rm = T)
+  pH100 = colMeans(Hout, na.rm = T)
   
   goals = cbind(
     Rmax = range(which(pR >= Rmax_prob)) * 1000,
     msy = range(which(pH >= msy_prob)) * 1000); rownames(goals) = c("lower", "upper")
   
-  probs = cbind(Rmax = pR, msy = pH); rownames(probs) = as.numeric(colnames(Rout)) * 1000
+  probs = cbind(Rmax = pR, msy = pH, H100 = pH100); rownames(probs) = as.numeric(colnames(Rout)) * 1000
   list(
     goals = goals,
     probs = probs
