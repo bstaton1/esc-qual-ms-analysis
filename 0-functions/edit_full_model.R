48a766545263f9fc115a3eefe64a27b7aaf355bf
Ben Staton <bstaton@critfc.org>
2019-08-22 21:18:36 -0700

    remove unnecessary arguments and operations

--- 0-functions/edit_full_model.R
+++ 0-functions/edit_full_model.R
@@ -1,4 +1,5 @@
-edit_full_model = function(model_lines, outfile, eggs = T, eggs_trend = T, sex_trend = T, mat_trend = T, vuln_trend = T, keep_whitespace = F, keep_comments = F) {
+
+edit_full_model = function(model_lines, outfile, len_trend = T, sex_trend = T, age_trend = T, keep_whitespace = F, keep_comments = F) {
   
   # 1.) replace function() with model {
   model_lines[model_lines == "function() {"] = "model {"
@@ -13,34 +14,17 @@ edit_full_model = function(model_lines, outfile, eggs = T, eggs_trend = T, sex_t
     model_lines = model_lines[-which(stringr::str_detect(model_lines, "^  +#"))]
   } 
   
-  # 4.) remove "%_%" instances
+  # 4.) remove "%_%" instances, but keep the T() or I() part
   model_lines = stringr::str_remove(model_lines, " %_%")
   
   ### ALTERATIONS TO CODE BASED ON MODEL VERSION ###
   
-  # handle vuln: 
-    # vuln_trend == T: pearson coefficients informed using rlm from each individual year
-    # vuln_trend == F: pearson coefficients informed using mean over years for rlm
-  if (!vuln_trend) {
-    match = model_lines[stringr::str_detect(model_lines, "v_term_2\\[t,a,s,m\\] <- ")]
-    replacement = stringr::str_replace(match, pattern = "rlm\\[t,a,s,m\\]", replacement = "mean(rlm[1:nt,a,s,m])")
-    model_lines[which(model_lines == match)] = replacement
-  }
-  
-  # handle eggs: 
-    # eggs == T: R = ricker(eggs) * noise
-    # eggs == F: R = ricker(spawners) * noise
-  if (!eggs) {
-    match = model_lines[stringr::str_detect(model_lines, "log_Rmean1\\[y\\] <-")]
-    model_lines[which(model_lines == match)] = stringr::str_replace_all(match, "eggs", "S")
-  }
-  
-  # handle eggs_trend: 
-    # eggs_trend == T: annual fecundity uses year-specific length data
-    # eggs_trend == F: annual fecundity uses the mean across all years
-  if (!eggs_trend) {
-    match = model_lines[stringr::str_detect(model_lines, "eggs_tas\\[t,a,s\\] <-")]
-    replacement = stringr::str_replace(match, pattern = "fecund\\[t,a,s\\]", replacement = "mean(fecund[1:nt,a,s])")
+  # handle len_trend: 
+    # len_trend == T: annual z uses year-specific length data
+    # len_trend == F: annual z uses the mean across all years
+  if (!len_trend) {
+    match = model_lines[stringr::str_detect(model_lines, "Z_tas\\[t,a,s\\] <-")]
+    replacement = stringr::str_replace(match, pattern = "z\\[t,a,s\\]", replacement = "mean(z[1:nt,a,s])")
     model_lines[which(model_lines == match)] = replacement
   }
   
@@ -54,10 +38,10 @@ edit_full_model = function(model_lines, outfile, eggs = T, eggs_trend = T, sex_t
     model_lines[which(model_lines == match)] = paste(white_space, replace, sep = "")
   }
   
-  # handle sex_trend: 
-  # mat_trend == T: logistic maturation for each sex, time trend for brood year
-  # mat_trend == F: time invariant maturation (slopes not estimated but fixed at zero)
-  if (!mat_trend) {
+  # handle age_trend: 
+  # age_trend == T: baseline category logit maturation for each sex, time trend for brood year
+  # age_trend == F: time invariant maturation (slopes not estimated but fixed at zero)
+  if (!age_trend) {
     match = model_lines[stringr::str_detect(model_lines, "b1_mat\\[s,a\\] ~ ")]
     white_space = unlist(stringr::str_extract_all(match, "  +"))
     replace = "b1_mat[s,a] <- 0"
@@ -65,5 +49,6 @@ edit_full_model = function(model_lines, outfile, eggs = T, eggs_trend = T, sex_t
   }
   
   cat(paste0(model_lines, collapse = "\n"), file = outfile)
-  
 }
+
+
