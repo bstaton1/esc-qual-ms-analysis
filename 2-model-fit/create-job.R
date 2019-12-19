4c093bf5e8936a4a1214e94320ebd3fa80605277
Ben Staton <bstaton@critfc.org>
2019-12-19 00:48:04 -0800

    create an R script to generate a job file specific to a model and hopper node

---
+++ 2-model-fit/create-job.R
@@ -0,0 +1,35 @@
+
+##### THIS SCRIPT'S PURPOSE IS TO CREATE A TEMPORARY JOB FILE TO PASS TO QSUB #####
+# it accepts two command line arguments:
+  # $1: model
+  # $2: node (144 or 145)
+
+# read in the command line arguments
+args = commandArgs(trailingOnly = T)
+model = args[1]
+node = args[2]
+
+# stop if node incorrect
+if (!(node %in% c("144", "145"))) stop("The node must be one of: '144' or '145'")
+
+# stop if model incorrect
+if (!(model %in% read.csv("model-key.csv")$model)) stop("The requested model was not found in 'model-key.csv'")
+
+# read in the send job template
+code = readLines("0-job-template.sh")
+
+# replace which node to use: this is one "computer" on the super computer. It has ram, processors, etc.
+code = unlist(lapply(code, function(x) stringr::str_replace(x, "NODE", as.character(node))))
+
+# replace which model to run: this is a number; models defined in model-key.csv
+code = unlist(lapply(code, function(x) stringr::str_replace(x, "MODEL", as.character(model))))
+code = c(code, "")   
+
+# write out the job file
+file_name = paste0("0-job-model-", model, ".sh")
+
+# write the code to the file
+writeLines(code, file_name)
+
+# print a message
+cat("Job file created:", file_name)
