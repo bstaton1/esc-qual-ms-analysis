b3cd925306f00aa1a08f8248e0feeab521df420a
Ben Staton <statonbe@gmail.com>
2019-12-31 15:14:01 -0800

    improvements to id_model

--- 0-functions/id_model.R
+++ 0-functions/id_model.R
@@ -1,32 +1,52 @@
 
 # assign a model identifier based the saved meta data
-id_model = function(meta, include_rand_age = F, include_src = F) {
+id_model = function(meta, unit = T, trends = T, rand_age = F, src = F, ess = F) {
   with(meta, {
     
+    # blank id
+    id = ""
+    
     # extract and name the unit
-    unit = ifelse(z_unit == "fish_count", "N",
-                  ifelse(z_unit == "egg_count", "E", "EM"))
+    z_unit = ifelse(z_unit == "fish_count", "N",
+                  ifelse(z_unit == "egg_count", "E",
+                         ifelse(z_unit == "egg_mass", "EM", NA)))
     
-    # extract and name the trends
-    A = ifelse(age_trend, "A", "")
-    S = ifelse(sex_trend, "S", "")
-    L = ifelse(len_trend, "L", "")
-    trends = paste0(A, S, L, collapse = "")
-    trends = ifelse(trends == "", "0", trends)
+
+    # (if requested) include the z unit
+    if (unit) {
+      id = z_unit
+    }
     
-    # build the basic id
-    id = paste(unit, trends, sep = "-")
-    
+    # (if requested) include the trends
+    if (trends) {
+      # extract and name the trends
+      A = ifelse(age_trend, "A", "")
+      S = ifelse(sex_trend, "S", "")
+      L = ifelse(len_trend, "L", "")
+      trends = paste0(A, S, L, collapse = "")
+      trends = ifelse(trends == "", "0", trends)
+      id = paste(id, trends, sep = ifelse(nchar(id) != 0, "-", ""))
+    }
+
     # (if requested) include an identifier for if the model has dirichlet ages or not
-    if (include_rand_age) {
+    if (rand_age) {
       d = ifelse(rand_age, "D", "M")
-      id = paste(id, d, sep = "-")
+      id = paste(id, d, sep = ifelse(nchar(id) != 0, "-", ""))
     }
     
     # (if requested) include an identifier for the source of the coefficients to inform length-based z
-    if (include_src & unit != "N") {
+    if (src & z_unit != "N") {
       src = meta$z_src
-      id = paste(id, src, sep = "-")
+      id = paste(id, src, sep = ifelse(nchar(id) != 0, "-", ""))
+    }
+    
+    # (if requested) include an identifer for the weighting of age composition data
+    if (ess) {
+      esc = paste0("E_", substr(meta$esc_n_eff, 1, 1), str_extract(meta$esc_n_eff, "[:digit:][:digit:][:digit:]$"))
+      com = paste0("C_", substr(meta$com_n_eff, 1, 1), str_extract(meta$com_n_eff, "[:digit:][:digit:][:digit:]$"))
+      sub = paste0("S_", substr(meta$sub_n_eff, 1, 1), str_extract(meta$sub_n_eff, "[:digit:][:digit:][:digit:]$"))
+      ess = paste(esc, com, sub, sep = "-")
+      id = paste(id, ess, sep = ifelse(nchar(id) != 0, "-", ""))
     }
     id
   })
