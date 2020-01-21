a650b3278ab0aaa470061c95022dcd5306ce69d7
Ben Staton <statonbe@gmail.com>
2020-01-21 10:20:16 -0800

    update which models get supplements

--- 3-post-process/render-Detailed-output.R
+++ 3-post-process/render-Detailed-output.R
@@ -5,18 +5,18 @@ rm(list = ls(all = T))
 html_dir = "os-detailed"
 if (!dir.exists(html_dir)) dir.create(html_dir)
 
-models = c(4,7,8,10,11,12)
+# which models to produce supplements for
+models = c(1,3,6,5,4,10,12)
+n_models = length(models)
 model_ids = c("N-0", "N-ASL", "E-0", "E-L", "E-S", "E-A", "E-SL", "E-AL", "E-AS", "E-ASL", "EM-0", "EM-ASL")
-for (model in models) {
-  out_file = paste0("Detailed_Output_", model, ".html", sep = "")
+
+for (m in 1:n_models) {
+  models = c(1,3,6,5,4,10,12)
+  model = models[m]
+  
+  out_file = paste0("S", m+1, "_", model_ids[model], ".html", sep = "")
   rmarkdown::render(
     input = "Detailed_Output_template.Rmd",
     output_file = file.path(html_dir, out_file))
-  
-  # if you want to open each file when complete
-  # environment is cleared when running each document, so you have to specify the file location again
-  html_dir = "os-detailed"
-  out_file = file.path(html_dir, paste0("Detailed_Output_", model, ".html", sep = ""))
-  browseURL(out_file)
 }
 
