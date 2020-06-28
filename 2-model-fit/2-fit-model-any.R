48d25a9151bdbc8a1ee2a3684be2b69353fde31f
Ben Staton <statonbe@gmail.com>
2020-06-27 18:51:24 -0700

    Alter fit-model for local use

--- 2-model-fit/2-fit-model-any.R
+++ 2-model-fit/2-fit-model-any.R
@@ -5,6 +5,7 @@ rm(list = ls(all = T))
 # settings
 args = commandArgs(trailingOnly = T)
 model = as.numeric(args[1])
+model = 10
 
 # location of data files
 data_dir = "inputs"
@@ -27,9 +28,9 @@ verbose =     T  # print JAGS messages to console?
 silent =      F  # print post processing progress?
 seed =        9  # seed for initial value and mcmc sampling
 mcmc_vshort = F  # run with very short mcmc settings?
-mcmc_lshort = F  # run with less short mcmc settings?
+mcmc_lshort = T  # run with less short mcmc settings?
 mcmc_medium = F  # run with medium mcmc settings?
-mcmc_long =   T  # run with long mcmc settings?
+mcmc_long =   F  # run with long mcmc settings?
 calc_eq =     T  # calculate equilibrium quantities (based on fishing mortialities that provide msy and Rmax)?
 save_files =  T  # save output?
 rand_age =    F  # use dirichlet-distributed ages?
