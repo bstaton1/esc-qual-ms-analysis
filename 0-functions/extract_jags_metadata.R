969d69273917c47cbabf30c63111b8e67e03dbc9
Ben Staton <statonbe@gmail.com>
2020-07-27 16:37:10 -0700

    edit the storing of DIC information

--- 0-functions/extract_jags_metadata.R
+++ 0-functions/extract_jags_metadata.R
@@ -1,7 +1,6 @@
 extract_jags_metadata = function(post) {
   list(
-    pD = post$pD,
-    DIC = post$DIC,
+    DIC = c(pD = post$pD, DIC = post$DIC),
     mcmc.info = unlist(post$mcmc.info[c("n.chains", "n.adapt", "n.iter", "n.burnin", "n.thin")]),
     starttime = post$run.date,
     stoptime = Sys.time(),
