9ea81bfb6a6129b9c1339af532858b16618c5009
Ben Staton <statonbe@gmail.com>
2020-08-08 17:59:54 -0700

    edit rm-jobs.sh to remove progress and error files as well

--- 2-model-fit/rm-jobs.sh
+++ 2-model-fit/rm-jobs.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-files=$(ls 0-job-model*)
+jobs=$(ls 0-job-model*)
+progress=$(ls progress*)
+errors=$(ls errors*)
 
 echo "Model-specific job scripts found:"
-echo $files
+echo $jobs
+echo "Model-specific progress outputs found:"
+echo $progress
+echo "Model-specific error outputs found:"
+echo $errors
 sleep 0.5
 echo ""
 echo "Are you sure you wish to remove these files? (y/n)"
@@ -13,6 +19,8 @@ echo ""
 if [ $remove == "y" ]
 then
   rm 0-job-model*
+  rm progress*
+  rm errors*
   echo "  Okay, these files were removed (you're welcome)."
 else
   echo "  Okay, these files will be kept (you're welcome for asking)."
