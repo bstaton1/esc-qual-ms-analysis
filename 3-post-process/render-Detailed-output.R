617e5923f844b7a7d41d26b234256f1584b7be35
Ben Staton <statonbe@gmail.com>
2020-08-29 10:49:31 -0700

    Add model E-AS to detailed supplements

--- 3-post-process/render-Detailed-output.R
+++ 3-post-process/render-Detailed-output.R
@@ -9,7 +9,7 @@ html_dir = "os-detailed"
 if (!dir.exists(html_dir)) dir.create(html_dir)
 
 # which models to produce supplements for
-models = c(1,3,6,5,4,10,12)
+models = c(1,3,6,5,4,9,10,12)
 n_models = length(models)
 model_ids = c("N-0", "N-ASL", "E-0", "E-L", "E-S", "E-A", "E-SL", "E-AL", "E-AS", "E-ASL", "EM-0", "EM-ASL")
 for (m in 1:n_models) {
